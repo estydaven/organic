b63fadc44438730a8c415b094a2a81b917eef520
estydaven <esetnod1111@gmail.com>
2022-08-15 20:04:13 +0300

    add slider section

---
+++ js/main.js
@@ -0,0 +1,7 @@
+$('.slider').slick({
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    prevArrow: '<div class="slider__arrow slider__arrow_prev"></div>',
+    nextArrow: '<div class="slider__arrow slider__arrow_next"></div>',
+});
