0ac2103039f95735733ed8995c0da84ed583c7d5
estydaven <esetnod1111@gmail.com>
2022-09-22 16:12:32 +0300

    add burger menu, arrop up, mobile styles

--- js/main.js
+++ js/main.js
@@ -5,3 +5,29 @@ $('.slider').slick({
     prevArrow: '<div class="slider__arrow slider__arrow_prev"></div>',
     nextArrow: '<div class="slider__arrow slider__arrow_next"></div>',
 });
+
+const menu = document.querySelector('.navigate');
+const burger = document.querySelector('.burger');
+const navLinks = document.querySelectorAll('.nav__link');
+
+function toggleMenu() {
+    menu.classList.toggle('navigate_active');
+    burger.classList.toggle('burger_active');
+}
+
+burger.addEventListener('click', toggleMenu);
+navLinks.forEach((el) => {
+    el.addEventListener('click', toggleMenu);
+})
+
+const arrowUp = document.querySelector('.up');
+
+function scrollUp() {
+    if (window.pageYOffset > 500) {
+        arrowUp.style.display = 'flex';
+    } else {
+        arrowUp.style.display = 'none';
+    }
+}
+
+window.addEventListener('scroll', scrollUp);
